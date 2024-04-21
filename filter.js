999c911228be6e5524be038a5c29436a9139a95c
Nada Tarkhan <70860125+Nadatarkhan@users.noreply.github.com>
2024-04-21 17:20:43 -0400

    t

--- filter.js
+++ filter.js
@@ -107,7 +107,7 @@ document.addEventListener('DOMContentLoaded', function() {
                     .style('width', width + 'px')
                     .append('svg')
                     .attr('width', width + margin.left + margin.right)
-                    .attr('height', 30)
+                    .attr('height', 40)
                     .append('g')
                     .attr('transform', 'translate(' + margin.left + ',' + 10 + ')')
                     .call(slider);
