6405693f00fabe44561cbaa618c8d70e2695d796
Nada Tarkhan <70860125+Nadatarkhan@users.noreply.github.com>
2024-04-22 19:29:18 -0400

    t

--- filter.js
+++ filter.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     const container = document.getElementById('innovation_plot'); // Get the container for innovation plot
     if (container) {
-        const margin = { top: 10, right: 30, bottom: 10, left: 20 },
+        const margin = { top: 10, right: 30, bottom: 30, left: 40 },
             width = container.clientWidth - margin.left - margin.right,
-            height = 40 - margin.top - margin.bottom;
+            height = 70 - margin.top - margin.bottom;
 
         // Load the data
         d3.csv("data/example_data.csv").then(function(data) {
