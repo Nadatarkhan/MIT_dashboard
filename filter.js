7079d984249f554d6711b14261b3050b559326a4
Nada Tarkhan <70860125+Nadatarkhan@users.noreply.github.com>
2024-04-22 20:13:51 -0400

    t

--- filter.js
+++ filter.js
@@ -98,7 +98,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 // Add the y Axis and style tick labels
                 svg.append("g")
                     .call(d3.axisLeft(y))
-                    .selectAll("")  // Select all text elements for the ticks
+                    .selectAll(".tick text")  // Select all text elements for the ticks
                     .style("font-size", "0px");  // Set the font size
 
 
