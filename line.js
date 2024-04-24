30716836b182ee3828992a0bb274909f030e67f5
Nada Tarkhan <70860125+Nadatarkhan@users.noreply.github.com>
2024-04-24 07:42:41 -0400

    t

--- line.js
+++ line.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const context = canvas.node().getContext("2d");
         context.scale(dpi, dpi);
 
-        const margin = { top: 40, right: 40, bottom: 60, left: 80 },
+        const margin = { top: 40, right: 40, bottom: 60, left: 100 },
             width = containerWidth - margin.left - margin.right,
             height = containerHeight - margin.top - margin.bottom;
 
