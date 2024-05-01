ef9b21b51df30c187deca1af51bde8770a1bab98
Nada Tarkhan <70860125+Nadatarkhan@users.noreply.github.com>
2024-05-01 12:24:35 -0400

    t

--- line.js
+++ line.js
@@ -144,7 +144,7 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
 
-        // Place these functions at the top or just before your scenario handling code
+        // reset buttons
         function resetBaselineFilters() {
             document.querySelectorAll(`input[name$="Filter"][value="baseline"]`).forEach(checkbox => {
                 checkbox.checked = false;
@@ -187,26 +187,15 @@ document.addEventListener('DOMContentLoaded', function() {
                 this.textContent = baselineActive ? "Deactivate Scenario" : "Activate Scenario";
 
                 const scenarioValue = 'baseline';  // This should be the identifier for the scenario
+
                 // Toggle baseline filters
-                document.querySelectorAll(`input[name$="Filter"][value="${scenarioValue}"]`).forEach(checkbox => {
-                    checkbox.checked = baselineActive;
-                    const field = checkbox.id.split('-')[0];
+                if (baselineActive) {
+                    filters[scenarioValue] = ['active'];
+                } else {
+                    delete filters[scenarioValue]; // Remove the scenario from filters if deactivated
+                }
 
-                    if (!filters[field]) {
-                        filters[field] = [];
-                    }
-
-                    if (baselineActive && !filters[field].includes(scenarioValue)) {
-                        filters[field].push(scenarioValue);
-                    } else if (!baselineActive) {
-                        filters[field] = filters[field].filter(v => v !== scenarioValue);
-                        if (filters[field].length === 0) {
-                            delete filters[field];  // Clean up if no more filters
-                        }
-                    }
-                });
-
-                // Additional logic to handle grid checkboxes
+                // Toggle additional logic to handle grid checkboxes
                 const gridFilters = ['bau', 'cheap_ng', 'decarbonization'];  // Assuming these are the IDs or values for the grid checkboxes
                 gridFilters.forEach(filter => {
                     document.querySelectorAll(`input[name="gridFilter"][value="${filter}"]`).forEach(checkbox => {
@@ -375,24 +364,21 @@ document.addEventListener('DOMContentLoaded', function() {
             context.save();
             context.translate(margin.left, margin.top);
 
-            let lastScenario = null; // Variable to track the last scenario processed
             filteredData.forEach((d, i) => {
                 if (i > 0 && d.scenario === filteredData[i - 1].scenario) {
-                    context.beginPath(); // Start a new path for each line segment
+                    context.beginPath();
                     context.moveTo(x(filteredData[i - 1].year), y(filteredData[i - 1].emission));
                     context.lineTo(x(d.year), y(d.emission));
-                    // Determine the color and thickness of the line based on the active filters
-                    const { color, lineWidth } = getColor(d.scenario, filters[d.scenario] && filters[d.scenario].length > 0);
-                    context.strokeStyle = color;
-                    context.lineWidth = lineWidth;
-                    context.stroke(); // Execute the drawing
+                    context.strokeStyle = getColor(d.scenario, filters[d.scenario] && filters[d.scenario].includes('active'));
+                    context.lineWidth = filters[d.scenario] && filters[d.scenario].includes('active') ? 2 : 0.5;
+                    context.stroke();
                 }
-                lastScenario = d.scenario; // Update the last scenario
             });
 
             context.restore();
             drawAxis();
         }
+
 
 
         function getColor(scenario, isActive) {
