cf41ff49f8857deafb0901e42abf043eb5f0349d
Nada Tarkhan <70860125+Nadatarkhan@users.noreply.github.com>
2024-05-05 16:47:56 -0400

    t

--- line.js
+++ line.js
@@ -519,10 +519,10 @@ document.addEventListener('DOMContentLoaded', function() {
                     context.moveTo(x(filteredData[i - 1].year), y(filteredData[i - 1].emission));
                     context.lineTo(x(d.year), y(d.emission));
 
-                    // Converting scenario to string for key comparison
+                    // Debugging output to check scenario type and filters
                     const scenarioKey = String(d.scenario);
                     const isActive = filters[scenarioKey] && filters[scenarioKey].includes('baseline');
-                    console.log(`Scenario: ${scenarioKey}, isActive: ${isActive}, Filters: ${filters[scenarioKey] ? filters[scenarioKey].join(', ') : 'None'}`);
+                    console.log(`Scenario: ${scenarioKey}, Type: ${typeof scenarioKey}, isActive: ${isActive}, Filters: ${filters[scenarioKey] ? filters[scenarioKey].join(', ') : 'None'}`);
 
                     context.strokeStyle = isActive ? '#b937b8' : '#565656';
                     context.lineWidth = isActive ? 2 : 0.9;
@@ -535,8 +535,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
 
 
-
-
+        
 
         function getColor(scenario, isActive) {
             // Custom function to determine color and lineWidth based on scenario and isActive flag
