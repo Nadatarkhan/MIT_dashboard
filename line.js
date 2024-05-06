1a21edc3f79f494956d2fa42afc0189d2185bb2c
Nada Tarkhan <70860125+Nadatarkhan@users.noreply.github.com>
2024-05-05 22:17:34 -0400

    t

--- line.js
+++ line.js
@@ -553,7 +553,7 @@ document.addEventListener('DOMContentLoaded', function() {
                     context.beginPath();
                     context.moveTo(x(filteredData[i - 1].year), y(filteredData[i - 1].emission));
                     context.lineTo(x(d.year), y(d.emission));
-                    if (isRecording) {
+                    if (isRecording && d.scenario === 'scenario2') {
                         context.strokeStyle = 'blue';
                         context.lineWidth = 2;
                         recordedLines.push({start: filteredData[i - 1], end: d, color: 'blue', lineWidth: 2});
